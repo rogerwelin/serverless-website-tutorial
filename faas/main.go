4d15cada3cbf33fa9fc08e08e0ee2b61882fc5cc
Roger Welin <roger.welin@icloud.com>
2019-03-28 17:40:52 +0100

    wip

--- faas/main.go
+++ faas/main.go
@@ -4,17 +4,25 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
-
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
+	"sync"
 )
 
 const apiUrl = "https://www.metaweather.com/api/location/"
 
-type Event struct {
-	WoeID string `json:"woeid"`
-}
+var (
+	woeIDS = []string{"906057",
+		"2487956",
+		"44418",
+		"638242",
+		"2295420",
+		"2151330",
+		"615702",
+		"721943",
+		"727232",
+		"766273"}
+)
 
 type rawWeatherData struct {
 	ConsolidatedWeather []struct {
@@ -30,7 +38,12 @@ type rawWeatherData struct {
 	LattLong     string `json:"latt_long"`
 }
 
+type AggregatedWeather struct {
+	WeatherItems []WeatherData `json:"weather_items"`
+}
+
 type WeatherData struct {
+	Woeid            int     `json:"woeid"`
 	WeatherStateName string  `json:"weather_state_name"`
 	MinTemp          float64 `json:"min_temp"`
 	MaxTemp          float64 `json:"max_temp"`
@@ -38,7 +51,7 @@ type WeatherData struct {
 	LattLong         string  `json:"latt_long"`
 }
 
-func returnHighestPredictability(data *rawWeatherData) (*WeatherData, error) {
+func returnHighestPredictability(data *rawWeatherData) (WeatherData, error) {
 	highestPredictability := 0
 	var predictabilityIndex int
 
@@ -49,7 +62,8 @@ func returnHighestPredictability(data *rawWeatherData) (*WeatherData, error) {
 		}
 	}
 
-	cleanedData := &WeatherData{
+	cleanedData := WeatherData{
+		Woeid:            data.Woeid,
 		WeatherStateName: data.ConsolidatedWeather[predictabilityIndex].WeatherStateName,
 		MinTemp:          data.ConsolidatedWeather[predictabilityIndex].MinTemp,
 		MaxTemp:          data.ConsolidatedWeather[predictabilityIndex].MaxTemp,
@@ -60,64 +74,57 @@ func returnHighestPredictability(data *rawWeatherData) (*WeatherData, error) {
 	return cleanedData, nil
 }
 
-func HandleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var weather *WeatherData
-	var event Event
+func (ag *AggregatedWeather) fetchApiData(woeID string, wg *sync.WaitGroup, mu *sync.Mutex) {
+	defer wg.Done()
 
-	resp := events.APIGatewayProxyResponse{Headers: make(map[string]string)}
-	resp.Headers["Access-Control-Allow-Origin"] = "*"
+	var weather WeatherData
+	var data rawWeatherData
 
-	fmt.Println("Request body: " + req.Body)
-
-	err := json.Unmarshal([]byte(req.Body), &event)
+	response, err := http.Get(apiUrl + woeID)
 	if err != nil {
-		resp.StatusCode = 500
-		resp.Body = err.Error()
-		return resp, nil
-	}
-
-	response, err := http.Get(apiUrl + event.WoeID)
-	if err != nil {
-		resp.StatusCode = 500
-		resp.Body = err.Error()
-		return resp, nil
-
+		return
 	}
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		resp.StatusCode = 500
-		resp.Body = err.Error()
-		return resp, nil
+		return
 	}
 	defer response.Body.Close()
 
-	var data rawWeatherData
 	if err := json.Unmarshal(body, &data); err != nil {
-		resp.StatusCode = 500
-		resp.Body = err.Error()
-		return resp, nil
+		return
 	}
 
 	weather, err = returnHighestPredictability(&data)
 	if err != nil {
-		resp.StatusCode = 500
-		resp.Body = err.Error()
-		return resp, nil
+		return
 	}
 
-	re, err := json.Marshal(*weather)
-	if err != nil {
-		resp.StatusCode = 500
-		resp.Body = err.Error()
-		return resp, nil
+	mu.Lock()
+	ag.WeatherItems = append(ag.WeatherItems, weather)
+	mu.Unlock()
+}
+
+func handleRequest() error {
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+	ag := AggregatedWeather{}
+
+	for _, item := range woeIDS {
+		wg.Add(1)
+		go ag.fetchApiData(item, &wg, &mu)
 	}
 
-	resp.StatusCode = 200
-	resp.Body = string(re)
-	return resp, nil
+	wg.Wait()
+	re, _ := json.Marshal(ag)
+	fmt.Println(string(re))
+	return nil
+
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	err := handleRequest()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
