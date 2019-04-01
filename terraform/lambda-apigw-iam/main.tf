cf3dd9b2db5ba7b57cc83ed5a787aeb7105df289
Roger Welin <roger.welin@icloud.com>
2019-04-01 12:12:47 +0200

    remodel lambda

--- terraform/lambda-apigw-iam/main.tf
+++ terraform/lambda-apigw-iam/main.tf
@@ -71,123 +71,22 @@ resource "aws_lambda_function" "weather_api" {
 }
 
 resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
+  statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.weather_api.arn}"
-  principal     = "apigateway.amazonaws.com"
+  principal     = "events.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.weather_gw.execution_arn}/*/*"
+  source_arn = "${aws_cloudwatch_event_rule.cron.arn}"
 }
 
-############################################
-# API GATEWAY - Sets up & configure api gw
-############################################
-
-resource "aws_api_gateway_rest_api" "weather_gw" {
-  name        = "weather-api"
-  description = "created by terraform"
+resource "aws_cloudwatch_event_rule" "cron" {
+  name                = "twice a day"
+  description         = "Runs twice a day"
+  schedule_expression = "cron(30 10 7,13 ? * * *)"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.weather_gw.id}"
-  parent_id   = "${aws_api_gateway_rest_api.weather_gw.root_resource_id}"
-  path_part   = "hello"
-}
-
-resource "aws_api_gateway_method" "options_method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.weather_gw.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_response" "options_200" {
-  rest_api_id = "${aws_api_gateway_rest_api.weather_gw.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "${aws_api_gateway_method.options_method.http_method}"
-  status_code = "200"
-
-  response_models {
-    "application/json" = "Empty"
-  }
-
-  response_parameters {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-
-  depends_on = ["aws_api_gateway_method.options_method"]
-}
-
-resource "aws_api_gateway_integration" "options_integration" {
-  rest_api_id = "${aws_api_gateway_rest_api.weather_gw.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "${aws_api_gateway_method.options_method.http_method}"
-  type        = "MOCK"
-
-  request_templates {
-    "application/json" = "{ \"statusCode\": 200 }"
-  }
-
-  depends_on = ["aws_api_gateway_method.options_method"]
-}
-
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = "${aws_api_gateway_rest_api.weather_gw.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "${aws_api_gateway_method.options_method.http_method}"
-  status_code = "${aws_api_gateway_method_response.options_200.status_code}"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  depends_on = ["aws_api_gateway_method_response.options_200"]
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.weather_gw.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_response" "200" {
-  rest_api_id = "${aws_api_gateway_rest_api.weather_gw.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-
-  depends_on = ["aws_api_gateway_method.proxy"]
-}
-
-resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.weather_gw.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.weather_api.invoke_arn}"
-  depends_on              = ["aws_api_gateway_method.proxy", "aws_lambda_function.weather_api"]
-}
-
-resource "aws_api_gateway_deployment" "gw_deploy" {
-  depends_on = [
-    "aws_api_gateway_integration.lambda",
-  ]
-
-  rest_api_id = "${aws_api_gateway_rest_api.weather_gw.id}"
-  stage_name  = "stage"
+resource "aws_cloudwatch_event_target" "clodwatch_event" {
+  rule      = "${aws_cloudwatch_event_rule.cron.name}"
+  target_id = "ebs_to_snapshot_backup"
+  arn       = "${aws_lambda_function.weather_api.arn}"
 }
